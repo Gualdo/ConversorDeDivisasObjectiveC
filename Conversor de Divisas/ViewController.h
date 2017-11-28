b81a2e689cd34848960a704cae5145280f05ad23
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2017-11-28 10:55:03 -0400

    All done

--- Conversor de Divisas/ViewController.h
+++ Conversor de Divisas/ViewController.h
@@ -10,6 +10,14 @@
 
 @interface ViewController : UIViewController
 
+// MARK: - Outlets
+
+@property (weak, nonatomic) IBOutlet UITextField *euroTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+// MARK: - Actions
+
+- (IBAction)ConvertirButtonPressed:(UIButton *)sender;
 
 @end
 
--- Conversor de Divisas/ViewController.m
+++ Conversor de Divisas/ViewController.m
@@ -14,16 +14,33 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+// MARK: - Global Variables
+
+const float EUR_TO_DOLAR = 1.18955; // Constante
+
+// MARK: - ViewController life cicle
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+// MARK: - Buttons Actions
+
+- (IBAction)ConvertirButtonPressed:(UIButton *)sender
+{
+    float euroValue = [self.euroTextField.text floatValue];
+    float dolarValue = euroValue * EUR_TO_DOLAR;
+    
+    [self.resultLabel setText: [NSString stringWithFormat: @"%.2f€ = %.2f$", euroValue, dolarValue]];
+    self.resultLabel.hidden = NO;
+}
 
 @end
